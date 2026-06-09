eb01a9eab9cf21f3a003ace5b95d5eacdac00bfe
ngziyi868-tech <ngziyi868@gmail.com>
2026-06-09 13:47:34 -0600

    CRUD

--- controllers/authController.js
+++ controllers/authController.js
@@ -71,23 +71,68 @@ const login = async (req, res) => {
     });
 
     res.status(200).json({
-      message:"Login successful",
+      message: "Login successful",
       token,
-      user:{
+      user: {
         id: user._id,
         username: user.username,
         email: user.email,
-      }
-    })
+      },
+    });
   } catch (error) {
-    console.error("Login Error:",error);
+    console.error("Login Error:", error);
     res.status(500).json({
-      message:"Server Error"
-    })
+      message: "Server Error",
+    });
+  }
+};
+
+const updateUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { username, email, password } = req.body;
+
+    const user = await User.findById(id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    if (username) user.username = username;
+    if (email) user.email = email;
+
+    if (password) {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      user.password = hashedPassword;
+    }
+
+    await user.save();
+
+    res.status(200).json({ message: "User has updated successfully!!" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const user = await User.findByIdAndDelete(id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 };
 
 module.exports = {
   register,
   login,
+  updateUser,
+  deleteUser,
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { register, login } = require("../controllers/authController");
+const { register, login , updateUser, deleteUser} = require("../controllers/authController");
 
 router.post("/register", register);
 router.post("/login", login);
+router.patch("/update/:id", updateUser);
+router.delete("/delete/:id", deleteUser);
 
 module.exports = router;
